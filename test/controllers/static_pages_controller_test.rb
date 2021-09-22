737767712ebf5e37494f9b363d7109bd27a7ab03
anna bozio <anna.bozio@gmail.com>
2021-09-22 13:53:04 +0200

    add helper to static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,7 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{setup}"
+    assert_select "title", setup
   end
 
   test "should get help" do
